7fe91e50797f3b559f5436131b0625c42f086db2
jack <zuichao123@126.com>
2018-03-26 19:37:28 +0800

    adf

---
+++ src/cn/sunjian/arraylist/test/ArrayListTest.java
@@ -0,0 +1,28 @@
+package cn.sunjian.arraylist.test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import cn.sunjian.p.bean.Person;
+
+public class ArrayListTest {
+
+	public static void main(String[] args) {
+		
+		ArrayList al = new ArrayList();
+		al.add(new Person("lisi1",21));
+		al.add(new Person("lisi2",22));
+		al.add(new Person("lisi3",23));
+		al.add(new Person("lisi4",24));
+		
+		Iterator it = al.iterator();
+		
+		while(it.hasNext()){
+//			System.out.println(((Person) it.next()).getName());
+			Person p = (Person) it.next();
+			System.out.println(p.getAge()+"--"+p.getName());
+		}
+		
+	}
+
+}
