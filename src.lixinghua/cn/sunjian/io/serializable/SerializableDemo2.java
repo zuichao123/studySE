8ba663a3dc150e3c8d9ea2a8a757a2423fb93c81
jack <zuichao123@126.com>
2018-05-03 08:30:44 +0800

    update in 2018-05-03 08:30:30

---
+++ src.lixinghua/cn/sunjian/io/serializable/SerializableDemo2.java
@@ -0,0 +1,40 @@
+package cn.sunjian.io.serializable;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+/**
+ * 对象序列化和反序列化操作练习
+ * 
+ * 		反序列化：
+ * 			对象输入流：ObjectIntputStream练习
+ * 
+ * @author jack
+ *
+ */
+
+public class SerializableDemo2 implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		File file = new File("c:"+File.separator+"serialzable.txt");//定义保存路径
+		
+		ObjectInputStream ois = new ObjectInputStream(
+				new FileInputStream(file));//实例化对象输入流
+		
+		Object temp = ois.readObject();//读取对象
+		
+		ois.close();//关闭对象输入流
+		
+		System.out.println(""+temp.toString());//打印内容
+
+	}
+
+}
