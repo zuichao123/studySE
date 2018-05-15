41ef65d8d9e1222dcaad5bb0c6ce1e6bc7f8e577
jack <zuichao123@126.com>
2018-05-15 08:43:07 +0800

    update in 2018-05-15 08:43:30

---
+++ src.lixinghua/cn/sunjian/enumeration/AbstractEnumDemo.java
@@ -0,0 +1,36 @@
+package cn.sunjian.enumeration;
+/**
+ * 使用枚举实现一个接口:
+ *		在枚举中定义抽象方法
+ *		枚举类型可以跟普通的类一样实现一个接口，但是实现接口的时候要求枚举中的每个对象都必须单独覆写好接口中的抽象方法。
+ *
+ * @author jack
+ *
+ */
+enum Color9 implements Print{
+	RED{
+		public String getColor(){
+			return "红色";
+		}
+	},GREEN{
+		public String getColor(){
+			return "绿色";
+		}
+	},BLUE{
+		public String getColor(){
+			return "蓝色";
+		}
+	};
+	public abstract String getColor();//枚举中定义抽象方法
+}
+
+public class AbstractEnumDemo {
+
+	public static void main(String[] args) {
+
+		for(Color8 c8 : Color8.values()){
+			System.out.print(c8+c8.getColor()+"、");
+		} 
+	}
+
+}
