edad8e8def49193ae3c3aeb03582124d59c705b7
zuichao <zuichao123@126.com>
2018-06-03 22:37:54 +0800

    update in 2018-06-03 22:37:30

--- src.lixinghua/cn/sunjian/gui/JFrameDemo.java
+++ src.lixinghua/cn/sunjian/gui/JFrameDemo.java
@@ -1,6 +1,8 @@
 package cn.sunjian.gui;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Point;
 
 import javax.swing.JFrame;
 
@@ -18,9 +20,16 @@ public class JFrameDemo {
 	public static void main(String[] args) {
 
 		JFrame f = new JFrame("第一个Swing窗体");
-		f.setSize(250, 80);//设置组件的大小
-		f.setBackground(Color.WHITE);//将背景设置成白色
-		f.setLocation(300, 200);//设置组件的显示位置
+		Dimension d = new Dimension();
+		
+		d.setSize(230, 80);//设置窗体的宽、高
+		
+		f.setSize(d);//设置组件的大小
+		f.setBackground(Color.BLACK);//将背景设置成白色
+		
+		Point p = new Point(300, 200);//设置窗体显示的x、y轴坐标
+		
+		f.setLocation(p);//设置组件的显示位置
 		f.setVisible(true);//设置组件可见
 	}
 
